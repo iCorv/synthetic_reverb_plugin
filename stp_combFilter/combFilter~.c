d2105192be79e4f5ad0209d4d09276673e09dd20
alberto <a.monciero@campus.tu-berlin.de>
2018-06-03 14:56:10 +0200

    linear interpolation and 2Comb patch added

--- stp_combFilter/combFilter~.c
+++ stp_combFilter/combFilter~.c
@@ -1,4 +1,6 @@
 #include "m_pd.h"
+#include <math.h>
+#include <stdlib.h>
 
 #define BUFFSIZE 44100
 
@@ -9,14 +11,17 @@ typedef struct combFilter_tilde
     t_object  x_obj;
     t_sample f;
     t_outlet* x_out;
-    t_int rptr;
+    t_float rptr;
     t_int wptr;
 	t_sample* buffer;
-	t_sample delayOut;
 	t_float fb;
 	t_sample lpOut;
 	t_float damp;
 	t_sample last_sample;
+	t_sample dOut;
+	t_float alpha;
+	t_int rpi;
+	t_float delaySample;
 
 } combFilter_tilde;
 
@@ -31,15 +36,22 @@ t_int *combFilter_tilde_perform(t_int *w)
 
     for(i=0; i<n; i++) {
 
-    		out[i] = in[i] + x-> fb * x-> lpOut;
-    		x->buffer[x->wptr++]  = out[i];
-    		x-> delayOut = x->buffer[x->rptr++];
-            x->lpOut = (1-x->damp) * x->delayOut + x-> damp * x->last_sample;
+    		out[i]= in[i] + (x->fb * x->lpOut);
+    		x->buffer[x->wptr]=out[i];
+    		x->rptr = x->wptr - x->delaySample;
+    		if (x->rptr < 0){
+    			x->rptr+=BUFFSIZE-1;
+    		}
+    		x->rpi = floor(x->rptr);
+    		x->alpha = x->rptr - x->rpi;
+    		x->dOut = x->alpha * x->buffer[x->rpi] + (1-x->alpha) * x->buffer[x->rpi+1];
+
+            x->lpOut = (1-x->damp) * x->dOut + x->damp * x->last_sample;
             x->last_sample = x->lpOut;
 
-    		if (x->wptr>=(BUFFSIZE-1)) {x->wptr = 0;}
-        	if (x->rptr>=(BUFFSIZE-1)) {x->rptr = 0;}
-        }
+    		if (x->wptr>=(BUFFSIZE-1)) x->wptr = 0; else x->wptr+=1;
+    }
+
 
     /* return a pointer to the dataspace for the next dsp-object */
     return (w+5);
@@ -64,20 +76,22 @@ void *combFilter_tilde_new(t_floatarg f)
     x->x_out = outlet_new(&x->x_obj, &s_signal);
     x-> rptr = 0;
     x-> wptr = 0;
-	x-> delayOut = 0;
 	x-> fb = 0;
 	x-> lpOut = 0;
-	x-> damp = 0.5;
+	x-> damp = 0.1;
+	x-> dOut = 0;
 	x-> last_sample = 0;
+	x-> alpha = 0;
+	x-> rpi = 0;
+	x-> delaySample = 0;
     x->buffer = (t_sample *) calloc (BUFFSIZE , sizeof(t_sample));
     return (void *)x;
 }
 
-void combFilterDelay_set(combFilter_tilde *x, int delay)
+void combFilterDelay_set(combFilter_tilde *x, float delay)
 {
 	if (delay < 0 || delay>=(BUFFSIZE-1)) delay = 0;
-    x->rptr = delay;
-
+	x->delaySample = delay;
 }
 
 void combFilterFeedback_set(combFilter_tilde *x, float fb)
@@ -93,7 +107,6 @@ void combFilterLowpass_set(combFilter_tilde *x, t_float fc)
 	if (fc < 0) fc = 0;
     x-> damp = fc;
 }
-
 
 void combFilter_tilde_setup(void)
 {
