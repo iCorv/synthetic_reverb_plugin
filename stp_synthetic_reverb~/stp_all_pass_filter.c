3565d872c1dbeb4002fdad5f8866fdeacabf591b
alberto <a.monciero@campus.tu-berlin.de>
2018-08-08 18:31:34 +0200

    documented allpass filter function

--- stp_synthetic_reverb~/stp_all_pass_filter.c
+++ stp_synthetic_reverb~/stp_all_pass_filter.c
@@ -13,42 +13,43 @@ stp_all_pass_filter* stp_all_pass_filter_new()
 {
     stp_all_pass_filter *x = (stp_all_pass_filter*)malloc(sizeof(stp_all_pass_filter));
     x-> gain = .0;
-    x->delayline1 = stp_delay_new(STANDARD_BUFFER_SIZE);
-    x->delayline2 = stp_delay_new(STANDARD_BUFFER_SIZE);
+    x->delay_line1 = stp_delay_new(STANDARD_BUFFER_SIZE);
+    x->delay_line2 = stp_delay_new(STANDARD_BUFFER_SIZE);
 
     return (void *)x;
 }
 void stp_all_pass_filter_free(stp_all_pass_filter *x)
 {
-    free(x-> delayline_out1);
-    free(x-> delayline_out2);
-    stp_delay_free(x-> delayline1);
-    stp_delay_free(x-> delayline2);
+    free(x-> delay_line_out1);
+    free(x-> delay_line_out2);
+    stp_delay_free(x-> delay_line1);
+    stp_delay_free(x-> delay_line2);
     free(x);
 }
 void stp_all_pass_filter_perform(stp_all_pass_filter *x, STP_INPUTVECTOR *in, STP_OUTPUTVECTOR *out, int vectorSize)
 {
     
     for (int i=0; i<vectorSize; i++){
-        out[i] = ((1+x->gain) * (x->delayline_out1[i])) - in[i] + x->delayline_out2[i] * x->gain;
+        out[i] = ((1+x->gain) * (x->delay_line_out1[i])) - in[i] + x->delay_line_out2[i] * x->gain;
     }
-    stp_delay_perform(x->delayline1, in, x->delayline_out1, vectorSize);
-    stp_delay_perform(x->delayline2, out, x->delayline_out2, vectorSize);
+    stp_delay_perform(x->delay_line1, in, x->delay_line_out1, vectorSize);
+    stp_delay_perform(x->delay_line2, out, x->delay_line_out2, vectorSize);
     
 }
 void stp_all_pass_filter_set_delay(stp_all_pass_filter *x, float _delay_in_samples)
 {
-    stp_delay_set_delay(x->delayline1, _delay_in_samples);
-    stp_delay_set_delay(x->delayline2, _delay_in_samples);
+    stp_delay_set_delay(x->delay_line1, _delay_in_samples);
+    stp_delay_set_delay(x->delay_line2, _delay_in_samples);
 }
-void stp_all_pass_filter_set_gain(stp_all_pass_filter *x, float g)
+void stp_all_pass_filter_set_gain(stp_all_pass_filter *x, float _gain)
 {
-    if (g >= 1) g = 1;
-    if (g <= 0) g = 0;
-    x-> gain = g;
+    if (_gain >= 1) _gain = 1;
+    if (_gain <= 0) _gain = 0;
+    x-> gain = _gain;
 }
 
 void stp_all_pass_filter_allocate_temp_buffer(stp_all_pass_filter *x, int vector_size) {
-    x->delayline_out1 = (float *) calloc (vector_size, sizeof(float));
-    x->delayline_out2 = (float *) calloc (vector_size, sizeof(float));
+	
+    x->delay_line_out1 = (float *) calloc (vector_size, sizeof(float));
+    x->delay_line_out2 = (float *) calloc (vector_size, sizeof(float));
 }
--- stp_synthetic_reverb~/stp_all_pass_filter.h
+++ stp_synthetic_reverb~/stp_all_pass_filter.h
@@ -1,10 +1,14 @@
-//
-//  stp_all_pass_filter.h
-//  stp_synthetic_reverb~
-//
-//  Created by C.Jaedicke on 07.06.18.
-//  Copyright © 2018 Intrinsic Audio. All rights reserved.
-//
+/**
+ * @file stp_delay.h
+ * @author C.Jaedicke, A.Monciero, P.Schuhladen, F.Müller <br>
+ * An interpolated delay <br>
+ * <br>
+ * @brief Audio Object for adding delay to the input<br>
+ * <br>
+ * stp_delay allows for delaying<br>
+ * any incoming audio signal. <br>
+ * <br>
+ */
 
 #ifndef stp_all_pass_filter_h
 #define stp_all_pass_filter_h
@@ -14,20 +18,83 @@
 #include <stdlib.h>
 #include "stp_delay.h"
 
+/**
+ * @struct stp_all_pass_filter
+ * @brief A structure for a allpass object <br>
+ * @var stp::delay_line1 First allpass filter delay line <br>
+ * @var stp::delay_line2 Second allpass filter delay line <br>
+ * @var stp::delay_line_out1 The buffer we save the first delay line output in <br>
+ * @var stp::delay_line_out2 The buffer we save the second delay line output in <br>
+ * @var stp::gain Adjustable gain value <br>
+ */
+ 
 typedef struct stp_all_pass_filter
 {
-    stp_delay *delayline1;
-    stp_delay *delayline2;
-    float *delayline_out1;
-    float *delayline_out2;
+    stp_delay *delay_line1;
+    stp_delay *delay_line2;
+    float *delay_line_out1;
+    float *delay_line_out2;
     float gain;
 } stp_all_pass_filter;
 
+/**
+ * @related stp_all_pass_filter
+ * @brief Creates a new allpass filter object<br>
+ * The function sets the standard values for the allpass filter. With delay buffer size = 44.1k <br>
+ * @return a pointer to the newly created stp_all_pass_filter object <br>
+ */
+
 stp_all_pass_filter* stp_all_pass_filter_new();
+
+/**
+ * @related stp_allpass_filter
+ * @brief Frees a allpass filter object<br>
+ * @param x My allpass filter object <br>
+ * The function frees the allocated memory<br>
+ * of a allpass filter object
+ */ 
 void stp_all_pass_filter_free(stp_all_pass_filter *x);
+
+/**
+ * @related stp_allpass_filter
+ * @brief Performs the allpass filter in realtime. <br>
+ * @param x My allpass filter object <br>
+ * @param in The input vector <br>
+ * @param out The output vector <br>
+ * @param vector_size The size of the i/o vectors <br>
+ * The function stp_allpass_filter_perform processes the <br>
+ * incoming signal with a allpass filter and copies the result to the output vector <br>
+ */
 void stp_all_pass_filter_perform(stp_all_pass_filter *x, STP_INPUTVECTOR *in, STP_OUTPUTVECTOR *out, int vectorSize);
+
+/**
+ * @related stp_allpass_filter
+ * @brief Sets the delay time in samples with floating point precision. <br>
+ * @param x My allpass filter object <br>
+ * @param _delay The delay in samples <br>
+ * Sets the delay time in samples with floating point precision. <br>
+ * Calls the stp_delay set delay function. <br>
+ */
 void stp_all_pass_filter_set_delay(stp_all_pass_filter *x, float _delay_in_samples);
-void stp_all_pass_filter_set_gain(stp_all_pass_filter *x, float g);
+
+/**
+ * @related stp_allpass_filter
+ * @brief Sets the gain value. <br>
+ * @param x My allpass filter object <br>
+ * @param _gain The gain <br>
+ * Sets the gain value with floating point precision. <br>
+ * Gain values exceeding 0 < gain < 1 are handelt by setting them to 0 or 1. <br>
+ */
+void stp_all_pass_filter_set_gain(stp_all_pass_filter *x, float _gain);
+
+/**
+ * @related stp_allpass_filter
+ * @brief Allocates temporary buffers to save output of other classes. <br>
+ * @param x My allpass filter object <br>
+ * @param vector_size The size of the i/o vectors <br>
+ * Allocates temporary buffers to save output of other classes. <br>
+ * Buffers for delay line output. <br>
+ */
 void stp_all_pass_filter_allocate_temp_buffer(stp_all_pass_filter *x, int vector_size);
 
 #endif /* stp_all_pass_filter_h */
